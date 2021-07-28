6c65271f3c775cde787b239dcde079848cf3d6d5
serbayacar <serbayacar@gmail.com>
2021-07-28 23:39:28 +0300

    Endpoints : init

--- src/application/controllers/getController.js
+++ src/application/controllers/getController.js
@@ -1,10 +1,26 @@
-// var url = require('url');
+const url = require('url');
 
 module.exports.getAsWeb =  function(req, res) {  
-    res.json("{ name : 'web-product1', production-date : '2021-07-13', exprire-date : '2022-01-01' }");
+    // This endpoint serves data as Web Page
+
+      // Parsing URL variables
+      const url_parts = url.parse(req.url, true);
+      const variables = url_parts.query;
+  
+      // Generating JSON Data
+      const data = "{ name : 'product-" +  variables.urunID +"', production-date : '2021-07-13', exprire-date : '2022-01-01' }"
+      res.json(data);
 }
 
 module.exports.getAsJson =  function(req, res) {  
-     
-    res.json("{ name : 'product1', production-date : '2021-07-13', exprire-date : '2022-01-01' }");
+    // This endpoint serves data as JSON
+
+
+    // Parsing URL variables
+    const url_parts = url.parse(req.url, true);
+    const variables = url_parts.query;
+
+    // Generating JSON Data
+    const data = "{ name : 'product-" +  variables.urunID +"', production-date : '2021-07-13', exprire-date : '2022-01-01' }"
+    res.json(data);
 }
