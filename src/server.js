5ab87ca2ca43d376224046301786076fd3e6fe8e
serbayacar <serbayacar@gmail.com>
2021-07-28 23:18:04 +0300

    Envorionment Vars :: init

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
-var express = require('express');
-var app= express();
+const dotenv = require("dotenv");
+
+const express = require('express');
+const app= express();
 
 // var parser = require('body-parser');
 // app.use(parser.urlencoded({extended: false}));
 // app.use(parser.json());
-
+dotenv.config();
 require('./application/routers/routerManager')(app);
 
-app.listen(3000, "127.0.0.1");
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("APP running on " + port);
+  });
+  
+// app.listen(3000, "127.0.0.1");
